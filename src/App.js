36f1a122bb4d20d00d631bdb8182b41601390a96
Shonn-Shonn <sshonn687@gmail.com>
2023-08-20 20:55:11 +0630

    add: second file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = "Wint Phyusin Maung";
+  return (
+    <div>
+       {name}{name}{name}
+    </div>
+  )
+}
+
+export default App;
