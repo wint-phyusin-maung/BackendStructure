445e660ece9adb650d9aac6f29ff2e62f4cab12c
Shonn-Shonn <sshonn687@gmail.com>
2023-08-20 21:33:58 +0630

    add: testing component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = "Wint Phyusin Maung";
+  
   return (
-    <div>
-       {name}{name}{name}
+    <div className='App'>
+       <Job salary={90000} position="Senior SDE" company="Amazon"/>
+       <Job salary={12000} position="Junior SDE"
+       company="Google"/>
+       <Job salary={10000} position="Project Manager"
+       company="Netflix"/>
     </div>
   )
 }
 
+
+const Job = (props) => {
+  return (
+    <div className='job'>
+      {props.salary}
+      {props.position}
+      {props.company}
+    </div>
+  )
+}
 export default App;
