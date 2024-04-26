6706bcf4b1df037507769c9db828a95761675ee1
wint-phyusin-maung <sshonn687@gmail.com>
2024-04-26 20:57:27 +0630

    add: user controller and route

---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+const User = require('../models/user');
+
+exports.createUser = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json({ user });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
